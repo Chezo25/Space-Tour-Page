c689d83e921c219d060b04d7664a8c36a6e50b7a
IslandBoyDev25 <chezling.stoddard@gmail.com>
2021-11-24 21:26:44 -0400

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,11 +1,16 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
+let tabFocus = 0;
+
 tabList.addEventListener('keydown', (e) => {
     const keydownLeft = 37;
     const keydownRight = 39;
 
     // change the tabindex of the current tab to -1
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
     
     // if the right key is pushed, move to the next tab on the right
     
